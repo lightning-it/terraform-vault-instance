a60c43f7f5d5415eca4cb93a0df0342658b10656
Dirk Egert <github@degert-it.de>
2026-07-28 15:09:39 +0200

    fix: add new parameters for transit usage to unseal vault

--- approle_secrets.tf
+++ approle_secrets.tf
@@ -12,30 +12,13 @@ resource "vault_approle_auth_backend_role" "cluster" {
   token_period       = each.value.token_period
   secret_id_ttl      = each.value.secret_id_ttl
   secret_id_num_uses = each.value.secret_id_num_uses
-
   depends_on = [
     vault_policy.policy,
     vault_mount.secret
   ]
 }
 
-# Secret IDs
-resource "vault_approle_auth_backend_role_secret_id" "cluster" {
-  for_each = var.bootstrap_phase >= 3 ? local.active.approles : {}
-
-  backend   = vault_auth_backend.approle[each.value.backend].path
-  role_name = each.value.role_name
-  namespace = each.value.namespace
-
-  lifecycle {
-    replace_triggered_by = [
-      vault_approle_auth_backend_role.cluster[each.key].secret_id_ttl,
-      vault_approle_auth_backend_role.cluster[each.key].secret_id_num_uses
-    ]
-  }
-}
-
-# KV Secrets mit role_id und secret_id
+# KV Secrets mit role_id
 resource "vault_kv_secret_v2" "approle_credentials" {
   for_each = var.bootstrap_phase >= 3 ? local.active.approles : {}
 
@@ -43,14 +26,12 @@ resource "vault_kv_secret_v2" "approle_credentials" {
   name      = each.value.credential_path
   namespace = each.value.namespace
 
-  # Dynamische Werte (role_id, secret_id) werden hier referenziert
+  # Dynamische Werte (role_id) werden hier referenziert
   data_json = jsonencode({
     role_id   = vault_approle_auth_backend_role.cluster[each.key].role_id
-    secret_id = vault_approle_auth_backend_role_secret_id.cluster[each.key].secret_id
   })
 
   depends_on = [
-    vault_mount.secret,
-    vault_approle_auth_backend_role_secret_id.cluster
+    vault_mount.secret
   ]
 }
